d279d677536905d4affdc303d41a4a69c59dc173
Gustavo Moura <gustavosilvamoura222@gmail.com>
2026-02-21 11:17:05 -0300

    Semana2

---
+++ semana2/Atividade1.c
@@ -0,0 +1,10 @@
+// Nome: Gustavo Silva De Moura - RA:
+// Fatec Ipiranga - Ads Vespertino 
+// Atividade PEM - Aula 2 - Dia 20/02/2025
+#include <stdio.h>
+#include <string.h>
+int main () {
+
+
+    return 0;
+}
