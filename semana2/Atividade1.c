28cfa44095abc8e681c0968e5b0e72c37cf66120
Gustavo Moura <gustavosilvamoura222@gmail.com>
2026-02-21 11:46:43 -0300

    Semana2

--- semana2/Atividade1.c
+++ semana2/Atividade1.c
@@ -1,10 +1,29 @@
 // Nome: Gustavo Silva De Moura - RA:2040482522023
 // Fatec Ipiranga - Ads Vespertino 
 // Atividade PEM - Aula 2 - Dia 20/02/2025
+// Criar um programa que calcular quantas palavras possui determinada frase.
+
 #include <stdio.h>
 #include <string.h>
-int main () {
 
+int main () {
+    char frase[200];
+    int i = 0, palavras = 0;
+
+    printf("Digite uma frase: ");
+    fgets(frase, sizeof(frase), stdin);
+
+    while (frase[i] != '\0') {
+
+        if ((frase[i] != ' ' && frase[i] != '\n') &&
+            (i == 0 || frase[i-1] == ' ')) {
+            palavras++;
+        }
+
+        i++;
+    }
+
+    printf("A frase possui %d palavras.\n", palavras);
 
     return 0;
 }
